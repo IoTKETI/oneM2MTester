9b53e96e8247d207ea938ac273c6c6372f281653
JaeYoungHwang <forest62590@gmail.com>
2017-09-18 21:17:38 -0700

    Modifying OneM2M_long2short_Enc.cc mapping file loading issues

--- bin/jsoncpp.d
+++
@@ -1,2 +0,0 @@
-jsoncpp.o jsoncpp.d : ../oneM2MTester/Lib/JSONCPP/jsoncpp.cc \
- ../oneM2MTester/Lib/JSONCPP/json/json.h
--- bin/tinyxml2.d
+++
@@ -1,2 +0,0 @@
-tinyxml2.o tinyxml2.d : ../oneM2MTester/Lib/TinyXml/tinyxml2.cc \
- ../oneM2MTester/Lib/TinyXml/tinyxml2.h
